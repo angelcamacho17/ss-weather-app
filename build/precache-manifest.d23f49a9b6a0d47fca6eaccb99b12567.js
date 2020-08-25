5040d3645ac96cd0683e1a3da690949f32fa500f
angelcamacho17 <angelcamacho1708@gmail.com>
2020-08-25 09:54:35 -0300

    adding

---
+++ build/precache-manifest.d23f49a9b6a0d47fca6eaccb99b12567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f324ab6a8173b60943456bde17b1829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4afe2de7e1ba2751e3ef",
+    "url": "/static/css/main.ca4aa508.chunk.css"
+  },
+  {
+    "revision": "278374423cee798828b3",
+    "url": "/static/js/2.8eed159e.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.8eed159e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afe2de7e1ba2751e3ef",
+    "url": "/static/js/main.8d26a37a.chunk.js"
+  },
+  {
+    "revision": "f8c7a5d48c189720d53e",
+    "url": "/static/js/runtime-main.ac14c711.js"
+  }
+]);
